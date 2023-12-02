07d14023ae3ddc3f177a904d8251a169f83398ff
matjanko <mateusz.jankowski@prokonto.pl>
2023-12-02 14:37:17 +0100

    Day One

--- 2023/01/Trebuchet/Program.cs
+++ 2023/01/Trebuchet/Program.cs
@@ -3,9 +3,7 @@
 var inputs = File.ReadAllLines("../../../Input.txt").ToList();
 var lines = inputs.Select(x => new CalibrationLine(x)).ToList();
 var sum = lines.Select(x => x.GetCalibrationValue()).Sum();
-
 Console.WriteLine("Part One: " + sum);
 
 var sum2 = lines.Select(x => x.GetCorrectedCalibrationValue()).Sum();
-
 Console.WriteLine("Part Two: " + sum2);
